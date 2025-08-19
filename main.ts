88ddc57f2a71326331e575390dd4bb9d77f2d240
MondragonFHub <mondragonfranco@gmail.com>
2025-08-19 18:57:39 +0200

    Uppdatera pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+finch.startFinch()
+finch.setTail(TailPort.One, 80, 40, 20)
+music.play(music.createSoundExpression(WaveShape.Noise, 5000, 0, 0, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 basic.forever(function () {
-	
+    if (finch.getDistance() == 15) {
+        finch.stopMotors()
+        finch.setTurn(RLDir.Right, 90, 30)
+    } else {
+        finch.startMotors(30, 30)
+    }
 })
